f18888001983bd486b13f5596014fde9e13892c2
GUGA <GUGA@MAC.local>
2021-04-23 23:49:15 +0300

    - Few tweaks before Homework#8

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-import {createPlayer} from './creators.js';
 import {player1, player2} from './characters.js';
 import {playerAttack, enemyAttack} from './attacks.js';
 import {generateLogs}from './battleLogs.js';
-import {showResult} from './results.js';
+import {showResult, gameStart} from './results.js';
 
 export const $arenas = document.querySelector('.arenas');
 export const $formFight = document.querySelector('.control');
 export const $chat = document.querySelector('.chat');
+export const $gameStart = document.querySelector('.start');
 export const HIT = {
     head: 30,
     body: 25,
     foot: 20};
 export const ATTACK = ['head', 'body', 'foot'];
 
+$formFight.style.display = 'none';
+
+
 $formFight.addEventListener('submit', function (e) {
     e.preventDefault();
-    document.getElementById("mortalkombat").play();
     const enemy = enemyAttack();
     const player = playerAttack();
 
@@ -29,20 +31,14 @@ $formFight.addEventListener('submit', function (e) {
         player2.changeHP(player.value);
         player2.renderHP();
         generateLogs('hit', player1, player2, player.value);
-
     }
     if (player.hit === enemy.defence) {
         generateLogs('defence', player1, player2);
-
     }
     if (enemy.hit === player.defence) {
         generateLogs('defence', player2, player1);
-
-
     }
     showResult();
 });
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
-generateLogs('start', player1, player2);
+gameStart()
