6d8efce2468911b78c15a210ac1b2a3e46857e8f
GUGA <GUGA@MAC.local>
2021-04-23 23:56:51 +0300

    - Fixed HP

--- characters.js
+++ characters.js
@@ -4,7 +4,7 @@ import {changeHP, elHP, renderHP} from './hpModifiers.js';
 export const player1 = {
     player: 1,
     name: 'Scorpion',
-    hp: 10,
+    hp: 100,
     img: 'assets/characters/scorpion.gif',
     weapon: ['Shotgun', 'Chainsaw'],
     attack: function () {
@@ -19,7 +19,7 @@ export const player1 = {
 export const player2 = {
     player: 2,
     name: 'Kitana',
-    hp: 10,
+    hp: 100,
     img: 'assets/characters/kitana.gif',
     weapon: ['Fan', 'Spoon'],
     attack: function () {
